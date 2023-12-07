bbbb6525e30d45dd75f2550fc3cac79def47485d
Theo Millard Tirtawinata <theo.millard@gmail.com>
2023-12-07 19:54:51 +0700

    multer err dependencies

--- routes/image.js
+++ routes/image.js
@@ -19,30 +19,30 @@ conn.once('open', () => {
     });
 });
 
-// const storage = new GridFsStorage({
-//     url: mongoURI,
-//     options: { useUnifiedTopology: true },
-//     file: (req, file) => {
-//         // this function runs every time a new file is created
-//         return new Promise((resolve, reject) => {
-//             // use the crypto package to generate some random hex bytes
-//             crypto.randomBytes(16, (err, buf) => {
-//                 if (err) {
-//                     return reject(err);
-//                 }
-//                 // turn the random bytes into a string and add the file extention at the end of it (.png or .jpg)
-//                 // this way our file names will not collide if someone uploads the same file twice
-//                 const filename = buf.toString('hex') + path.extname(file.originalname);
-//                 const fileInfo = {
-//                     filename: filename,
-//                     bucketName: 'images',
-//                 };
-//                 // resolve these properties so they will be added to the new file document
-//                 resolve(fileInfo);
-//             });
-//         });
-//     },
-// });
+const storage = new GridFsStorage({
+    url: mongoURI,
+    options: { useUnifiedTopology: true },
+    file: (req, file) => {
+        // this function runs every time a new file is created
+        return new Promise((resolve, reject) => {
+            // use the crypto package to generate some random hex bytes
+            crypto.randomBytes(16, (err, buf) => {
+                if (err) {
+                    return reject(err);
+                }
+                // turn the random bytes into a string and add the file extention at the end of it (.png or .jpg)
+                // this way our file names will not collide if someone uploads the same file twice
+                const filename = buf.toString('hex') + path.extname(file.originalname);
+                const fileInfo = {
+                    filename: filename,
+                    bucketName: 'images',
+                };
+                // resolve these properties so they will be added to the new file document
+                resolve(fileInfo);
+            });
+        });
+    },
+});
 
 // set up our multer to use the gridfs storage defined above
 const store = multer({
