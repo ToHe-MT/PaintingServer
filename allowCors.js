a55e21b0b44bf3e63cb1204f39880d85011acdf3
Theo Millard Tirtawinata <theo.millard@gmail.com>
2023-12-08 10:06:07 +0700

    cors

--- allowCors.js
+++ allowCors.js
@@ -1,23 +1,16 @@
-const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-        res.status(200).end()
-        return
+const allowCors = (req, res, next) => {
+    if (res) {
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+        res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
     }
-    next();
-}
 
-const handler = (req, res) => {
-    const d = new Date()
-    res.end(d.toString())
-}
+    if (req.method === 'OPTIONS' && res) {
+        res.status(200).end();
+        return;
+    }
+    next(); // Move to the next middleware/route handler
+};
 
-module.exports = allowCors(handler)
+module.exports = allowCors;
